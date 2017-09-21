07a457c25030468d82221cc84be62b63eded16f0
Dan <32112177+djh201@users.noreply.github.com>
2017-09-20 22:55:18 -0400

    Add files via upload

---
+++ Assignment1/sorter.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+/*
+plan:	read the first 'header file' for value types each entry will follow,
+		creat the list of all stuc valueTypes into valueList
+		once all values are in list, parse through by sorting
+things to remember:		memory allocation - dont free always initalize 
+						allocate valuType as needed, dont creat list with predefined size(user determines)
+						
+useful methods:		mergsort, fastest way to sort large datasets 
+					finding if valueType exists
+					printinf the list
+					debugging tools(flags, error checking etc)
+*/
+int void main(int *argc, char* arg[]){
+		FILE *file ; 
+
+}
