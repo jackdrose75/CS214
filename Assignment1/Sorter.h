51a9c9e5582dabdbfbe2c704321543a9f4db5f64
Dan <32112177+djh201@users.noreply.github.com>
2017-09-20 22:24:58 -0400

    Update Sorter.h

--- Assignment1/Sorter.h
+++ Assignment1/Sorter.h
@@ -5,9 +5,30 @@
 *
 *
 ******/
+/* valueType is how each row is represented.  W create one struct to define each row, the function each row has
+// the type of value for testing purposes is limited but can be expanded to fit larger files 
+  a user defined input on what to sort the file by will be given in command line -> global char? thi way we
+  always know what to sort 
+  */ 
 
-//Suggestion: define a struct that mirrors a record (row) of the data set
 
+typedef struct valueType{
+    char * color ;
+    int  * duration ;
+    int * director factor ;
+    char * generes ; 
+    struct valueType *ptr ; // a pointer to be able to assing values as read, used when reading in data 
+}
+/*when comparing a struct element to sort by some things to test: 
+//elments with same name/value,
+repeated elements, elements not present */
 
-//Suggestion: prototype a mergesort function
+/*next we need a list of all the different types of value we read from vsc
+We can have an array of structs of valueType.  this becomes our converted data to use
+all elements in this list must be intialized when reading in file. 
+*/
+typdef struct valueList{
+    struct valuetype *entry ; //my idea is a list is pointing to how many valueTypes we have
+    int size ; // useful for knowing size of list? maybe global variable instead, 
+}
 
