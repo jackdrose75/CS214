5716ddb1b14919b5f90ddf79e6a7b9eadb1608d6
quickbrownfox319 <quickbrownfox319@gmail.com>
2017-09-27 10:33:29 -0400

    working on parsing. running into segfault

--- Assignment1/sorter.c
+++ Assignment1/sorter.c
@@ -2,65 +2,65 @@
 #include<stdlib.h>
 #include<string.h>
 
-struct type{
- 
-    char* value ;
-};
+typedef struct movie_data{
+    char* cat_data; //holds category data (i.g. "Titanic" for category "movie_title")
+    char* row_array[]; //holds pointer to array of row.
+} Data;
 
-//used to save strings into struct array
-/*char *token_string(char *str, int sizeofArray){
-    struct type array_input;
+//used to parse a string line based on delimiter commas and quotations
+//builds character array (string) dynamically
+//returns pointer to built string
+char *parseStr(char *str){
+    
+    //build string as character array
+    int i = 0;
+    char c;
+    str = (char *)malloc(sizeof(char)); //build string as character array
 
+    if (str == NULL){
+        return NULL; 
+    }
+    //parse until end of line
+    while((c = getchar()) != '\n' && (c != ',') && (c != '\0')){
 
+        //if encounter opening quote
+        if(c == '\"'){
+            while((c = getchar()) != '\"') {
+                //realloc(str, (sizeof(char)));
+                str = realloc(str, (i*sizeof(char)));
+                str[i++] = c;
+            }
+        } else {
+            //
+            str = realloc(str, (sizeof(char)));
+            str[i++] = c;
+        }
+    }
 
+    str[i] = '\0';
+    printf("String contains %s\n", str);
+    return str;
 }
-*/
+
 
 int main(int argc, char **argv){
-
 
     //stores the input from the user as a string variable
     char *sort_type = argv[1]; //get argument to sort by, -c for column
     char *sort_topic = argv[2]; //get topic i.e. 'movies'
 
-/*
-    //catch if -c is not implemented
-    if (strcomp(sort_type, "") == 0) {
-        printf("Please specify type of sorting required. \n");
-        return 0;
 
-    //catch if movie not implemented
-    if (strcomp(sort_topic, "") == 0) {
-        printf("Please specify topic to sort by. \n");
-        return 0;
+    Data test;
+    printf("Here\n");
+    int num = 1; //to init array
+    Data **data_array = (Data **)malloc(num*sizeof(Data **)); //init data_array to single unit of type Data
+
+    //get input line by line from stdin
+    char str_line[1024]; //hold string line
+    while(fgets(str_line, 1024, stdin) != NULL){
+        printf("pass to parse function\n");
+        printf("%s\n", parseStr(str_line));
     }
-*/
-
-    //get stdin input
-    char temp[1000]; //buffer input 
-    //fgets(temp, 1000, stdin);
-    while(scanf("%s",temp) != EOF) {
-        //printf("%s\n", temp);        
-    }
-
-
-    char* ptr = temp;
-
-    //store an array holding column entry
-    struct type entry;
-    entry.value = temp ;
-
-    //tokenization using comma delimiter
-    char* tokenp ;
-    tokenp = strtok(entry.value, ",") ; //separate by comma
-    
-    // get categories
-    while(tokenp != NULL){
-        printf("%s \n", tokenp);
-        tokenp = strtok(NULL, ",") ;
-    }
-
-
 
     return  0;
 
