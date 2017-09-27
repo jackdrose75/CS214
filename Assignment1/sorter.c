0c1db3a7aaf3a4e6f53a208dd4edd31ff0eaf29c
quickbrownfox319 <quickbrownfox319@gmail.com>
2017-09-27 16:19:40 -0400

    parsing function goes by char by char, but hitting segfault. might need to switch how i store chars, as i'm resizing input rather than the stored element

--- Assignment1/sorter.c
+++ Assignment1/sorter.c
@@ -4,42 +4,29 @@
 
 typedef struct movie_data{
     char* cat_data; //holds category data (i.g. "Titanic" for category "movie_title")
-    char* row_array[]; //holds pointer to array of row.
+    char* row_array; //holds pointer to array of row.
 } Data;
 
 //used to parse a string line based on delimiter commas and quotations
 //builds character array (string) dynamically
 //returns pointer to built string
-char *parseStr(char *str){
-    
-    //build string as character array
-    int i = 0;
-    char c;
-    str = (char *)malloc(sizeof(char)); //build string as character array
+char *strParser(char *input){
+  int i = 0;
+  char c;
 
-    if (str == NULL){
-        return NULL; 
-    }
-    //parse until end of line
-    while((c = getchar()) != '\n' && (c != ',') && (c != '\0')){
+  if(input == NULL) {
+    return NULL;
+  }
+  
+  //traverse char by char of input
+  while((input[i] != '\n')) {
+    realloc(input, (sizeof(char)));
+    i++;
+  }
 
-        //if encounter opening quote
-        if(c == '\"'){
-            while((c = getchar()) != '\"') {
-                //realloc(str, (sizeof(char)));
-                str = realloc(str, (i*sizeof(char)));
-                str[i++] = c;
-            }
-        } else {
-            //
-            str = realloc(str, (sizeof(char)));
-            str[i++] = c;
-        }
-    }
+  input[i] = '\0';
+  printf("\nYou entered the string: %s\n", input);
 
-    str[i] = '\0';
-    printf("String contains %s\n", str);
-    return str;
 }
 
 
@@ -49,17 +36,15 @@ int main(int argc, char **argv){
     char *sort_type = argv[1]; //get argument to sort by, -c for column
     char *sort_topic = argv[2]; //get topic i.e. 'movies'
 
-
-    Data test;
-    printf("Here\n");
-    int num = 1; //to init array
-    Data **data_array = (Data **)malloc(num*sizeof(Data **)); //init data_array to single unit of type Data
+    int num = 1;
 
     //get input line by line from stdin
-    char str_line[1024]; //hold string line
+    char *str_line = (char *)malloc(num*sizeof(char *));
+
     while(fgets(str_line, 1024, stdin) != NULL){
-        printf("pass to parse function\n");
-        printf("%s\n", parseStr(str_line));
+        printf("About to enter for loop\n\n");
+        char *test = strParser(str_line);
+
     }
 
     return  0;
