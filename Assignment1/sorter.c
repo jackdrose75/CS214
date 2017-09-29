841caffa77e08c355cdfaaba8a1ed4cad2ea70c5
quickbrownfox319 <quickbrownfox319@gmail.com>
2017-09-28 21:13:16 -0400

    can parse lines. saves first row into categories and stores user defined index

--- Assignment1/sorter.c
+++ Assignment1/sorter.c
@@ -7,48 +7,40 @@ typedef struct movie_data{
     char* row_array; //holds pointer to array of row.
 } Data;
 
-//used to parse a string line based on delimiter commas and quotations
-//builds character array (string) dynamically
-//returns pointer to built string
-char *strParser(char *input){
-  int i = 0;
-  char c;
-
-  if(input == NULL) {
-    return NULL;
-  }
-  
-  //traverse char by char of input
-  //need to check for NULL to avoid segfault
-  while((input[i] != NULL)) {
-    realloc(input, (sizeof(char)));
-    i++;
-  }
-
-  input[i] = '\0';
-  printf("\nYou entered the string: \n%s\n", input);
-
-
-}
-
 
 int main(int argc, char **argv){
 
     //stores the input from the user as a string variable
     char *sort_type = argv[1]; //get argument to sort by, -c for column
     char *sort_topic = argv[2]; //get topic i.e. 'movies'
-
     int num = 1;
+
+    //get categories inputted by use
+    //match by index number of column
+    char *first_row = (char *)malloc(num*sizeof(char *));
+    char *first = first_row; //need to keep track for strsep
+    fgets(first_row, 1024, stdin);
+    char *found;
+    int count = 0;
+    while((found = strsep(&first, ",")) != NULL){
+        if(strcmp(found, sort_topic) == 0){
+            break;
+        }
+        count++;
+    }
+    free(first_row); //get rid of space used to hold first row
 
     //get input line by line from stdin
     char *str_line = (char *)malloc(num*sizeof(char *));
-
     while(fgets(str_line, 1024, stdin) != NULL){
-        printf("About to enter for loop\n\n");
-        char *test = strParser(str_line);
-
+        printf("%s\n", str_line);
+        
     }
 
     return  0;
 
 }
+
+
+
+
