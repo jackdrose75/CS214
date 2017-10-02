33d4bfeda06a080aca424a7c192242b0e471c24c
quickbrownfox319 <quickbrownfox319@gmail.com>
2017-10-02 12:25:39 -0400

    for testing gdb

--- Assignment1/sorter.c
+++ Assignment1/sorter.c
@@ -5,7 +5,7 @@
 
 //
 typedef struct{
-    char *category; //holds category data (i.g. "Titanic" for category "movie_title")
+    char *field_data; //holds category data (i.g. "Titanic" for category "movie_title")
     char *original_row; //holds pointer to array of row.
 } Record;
 
@@ -14,12 +14,14 @@ typedef struct{
 //returns an array
 char *getCat(char *input, int index){
     char *sub_str = NULL; //substring we want to return
-
     //check to see if input is null, else set substring ptr to input
-    if (input == NULL){
-        return NULL;
+    for(sub_str = strtok(input, ","); sub_str && *sub_str; sub_str = strtok(NULL, ",\n")){
+        if (!--index){
+            return sub_str;
+        }
     }
-    sub_str = input;
+
+    return NULL;
 
 }
 
@@ -36,31 +38,38 @@ int main(int argc, char **argv){
     char *first_row = (char *)malloc(sizeof(char *));
     char *_first_row = first_row; //need to keep track for strsep
     fgets(first_row, 1024, stdin); //get first row
-    char *field_data; //pulled category from strsep
-    int field_index = 0;
+    char *cat; //pulled category from strsep
+    int cat_index = 0;
     int row_size = 1;
  
-    while((field_data = strsep(&_first_row, ",")) != NULL){
-        if(strcmp(field_data, sort_topic) == 0){
-            field_index = row_size;  //index of where category is
-            printf("%s, %d\n", field_data, field_index);
+    while((cat = strsep(&_first_row, ",")) != NULL){
+        if(strcmp(cat, sort_topic) == 0){
+            cat_index = row_size;  //index of where category is
+            printf("%s, %d\n", cat, cat_index);
         }
     }
     free(first_row); //get rid of space used to hold first row
 
+
     //initialize array of structs
     Record **outputArray = (Record **)malloc(sizeof(Record **));
-    
+
     //get input line by line from stdin
     char *row = (char *)malloc(sizeof(char *));
+
+    printf("TEST\n");
+
     int index = 0;
     int size = 1;
     while(fgets(row, 1024, stdin) != NULL){
-        if (getCat(row, field_index) == NULL){
-            outputArray[index] -> category = NULL;
-        }
         outputArray[index] = (Record *)realloc(outputArray, size*sizeof(Record *));
-        outputArray[index] -> category = field_data;
+        char *tmp = getCat(row, cat_index);
+        if (!tmp){
+            outputArray[index] -> field_data = NULL;
+        } else {
+            outputArray[index] -> field_data = tmp;
+        }
+        printf("Saved field_data: %s\n", outputArray[index]);
         outputArray[index] -> original_row = row;
         size++;
     }
